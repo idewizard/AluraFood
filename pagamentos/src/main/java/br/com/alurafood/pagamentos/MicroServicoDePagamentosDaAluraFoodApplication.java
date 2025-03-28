8f1c5e4db2699c65bb1ebe44f4dda195581ae6bc
idewizard <andre.sousa.tec@gmail.com>
2025-03-28 00:02:35 -0300

    Comecando o microservico de Pagamentos do projeto AluraFood

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/MicroServicoDePagamentosDaAluraFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicoDePagamentosDaAluraFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicoDePagamentosDaAluraFoodApplication.class, args);
+	}
+
+}
